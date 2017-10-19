b6812adeb29bd6344e2d3b41533af9fee1cea212
wi15b189 <wi15b189@technikum-wien.at>
2017-10-19 12:54:13 +0200

    UE3

---
+++ StudentHandler/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHandler.ViewModels
+{
+    class MainViewModel
+    {
+        public StudentVm Student { get; set; }
+        public ObservableCollection<string> ProgramSelection { get; set; }
+    }
+}
---
+++ StudentHandler/ViewModels/StudentVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHandler.ViewModels
+{
+    class StudentVm
+    {
+        private int age;
+        private string lastname;
+        private string firstname;
+        private bool paid;
+        private string program;
+
+        public string Program
+        {
+            get { return program; }
+            set { program = value; }
+        }
+
+        public int Age { get => age; set => age = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public string Firstname { get => firstname; set => firstname = value; }
+        public bool Paid { get => paid; set => paid = value; }
+    }
+}
